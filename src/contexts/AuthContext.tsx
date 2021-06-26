0fa9892ce8ae6b062d8ce04ee995c1de306c9d1c
Cláudio Siqueira <claudio.filho_rv@hotmail.com>
2021-06-26 05:46:11 -0300

    feat: admin and guest rooms

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -50,20 +50,18 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
 
     const result = await auth.signInWithPopup(provider);
 
-    {
-      if (result.user) {
-        const { displayName, photoURL, uid } = result.user;
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
 
-        if (!displayName || !photoURL) {
-          throw new Error("Missing information from Google Account");
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL,
-        });
+      if (!displayName || !photoURL) {
+        throw new Error("Missing information from Google Account");
       }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+      });
     }
   }
 
